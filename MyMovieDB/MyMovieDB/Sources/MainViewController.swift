898566f1c7eead6fad7713094616f6f7feda1fdf
Chrystian Salgado <salgado.dev@gmail.com>
2019-05-24 17:28:10 -0300

    Refactor MainViewController file

--- MyMovieDB/MyMovieDB/Sources/MainViewController.swift
+++ MyMovieDB/MyMovieDB/Sources/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MyMovieDB
 //
 //  Created by Chrystian Salgado on 24/05/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

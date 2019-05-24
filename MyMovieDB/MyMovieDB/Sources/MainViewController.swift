df4703e184637b3488bc3f9d4f5bbb5a9d15f6ca
Chrystian Salgado <salgado.dev@gmail.com>
2019-05-24 17:53:14 -0300

    Criating localizable file

--- MyMovieDB/MyMovieDB/Sources/MainViewController.swift
+++ MyMovieDB/MyMovieDB/Sources/MainViewController.swift
@@ -12,7 +12,8 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
 
     private func configureUI() {

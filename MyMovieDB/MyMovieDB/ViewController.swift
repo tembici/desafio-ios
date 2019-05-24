e6ddd87f992501f165dea27c47ea4198512585da
Chrystian Salgado <salgado.dev@gmail.com>
2019-05-24 17:09:02 -0300

    Creating project

---
+++ MyMovieDB/MyMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieDB
+//
+//  Created by Chrystian Salgado on 24/05/19.
+//  Copyright © 2019 Salgado's Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

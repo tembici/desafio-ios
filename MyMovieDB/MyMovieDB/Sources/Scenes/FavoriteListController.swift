58ccf7bcadfb08ed97e544146151f7b5d5068832
Chrytian Salgado Pessoal <salgado.dev@gmail.com>
2019-06-08 12:23:47 -0300

    Adding table view controller to favorites, configurarion in progress

---
+++ MyMovieDB/MyMovieDB/Sources/Scenes/FavoriteListController.swift
@@ -0,0 +1,51 @@
+//
+//  FavoriteListController.swift
+//  MyMovieDB
+//
+//  Created by Chrytian Salgado Pessoal on 08/06/19.
+//  Copyright © 2019 Salgado's Production. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteListController: UITableViewController {
+
+    private let cellIdentifier = "favoriteCellIdentifier"
+    private var numberOfRows: Int = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return numberOfRows
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        return cell
+    }
+
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+ 
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}

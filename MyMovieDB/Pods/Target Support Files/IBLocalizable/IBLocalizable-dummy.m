2cd14178408892d89c7e041e33850bdf7a1b1a0c
Chrytian Salgado Pessoal <salgado.dev@gmail.com>
2019-06-07 22:46:08 -0300

    Delete pods folder

--- MyMovieDB/Pods/Target Support Files/IBLocalizable/IBLocalizable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IBLocalizable : NSObject
-@end
-@implementation PodsDummy_IBLocalizable
-@end
--- MyMovieDB/Pods/Target Support Files/Pods-MyMovieDB/Pods-MyMovieDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyMovieDB : NSObject
-@end
-@implementation PodsDummy_Pods_MyMovieDB
-@end

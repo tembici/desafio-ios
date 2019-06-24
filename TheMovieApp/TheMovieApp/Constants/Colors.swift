0fa2f3b9542ed960c3978ee4cca87fdc53275317
Wilson Kim <wilson.kim@foton.la>
2019-06-24 15:49:48 -0300

    Added launchScreen image

---
+++ TheMovieApp/TheMovieApp/Constants/Colors.swift
@@ -0,0 +1,28 @@
+//
+//  Colors.swift
+//  TheMovieApp
+//
+//  Created by Wilson Kim on 24/06/19.
+//  Copyright © 2019 WilsonKim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    public static var black: UIColor = UIColor.black
+    public static var darkGray: UIColor = UIColor.darkGray
+    public static var lightGray: UIColor = UIColor.lightGray
+    public static var white: UIColor = UIColor.white
+    public static var gray: UIColor = UIColor.gray
+    public static var red: UIColor = UIColor.red
+    public static var green: UIColor = UIColor.green
+    public static var blue: UIColor = UIColor.blue
+    public static var cyan: UIColor = UIColor.cyan
+    public static var yellow: UIColor = UIColor.yellow
+    public static var magenta: UIColor = UIColor.magenta
+    public static var orange: UIColor = UIColor.orange
+    public static var purple: UIColor = UIColor.purple
+    public static var brown: UIColor = UIColor.brown
+    public static var clear: UIColor = UIColor.clear
+}
---
+++ TheMovieApp/TheMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Wilson Kim on 24/06/19.
+//  Copyright © 2019 WilsonKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

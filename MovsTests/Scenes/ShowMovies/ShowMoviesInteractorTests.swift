2ac9839da8288357198e32968ec310f85cdd5050
Miguel Pimentel <miguelpimentel2013@gmail.com>
2019-09-25 11:46:45 -0300

    Added imprivements on unit test coverage

--- MovsTests/Scenes/ShowMovies/ShowMoviesInteractorTests.swift
+++ MovsTests/Scenes/ShowMovies/ShowMoviesInteractorTests.swift
@@ -1,58 +1,82 @@
-////
-////  ShowMoviesInteractorTests.swift
-////  Movs
-////
-////  Created by Miguel Pimentel on 25/09/19.
-////  Copyright (c) 2019 Miguel Pimentel. All rights reserved.
-////
 //
-//@testable import Movs
-//import XCTest
+//  ShowMoviesInteractorTests.swift
+//  Movs
 //
-//class ShowMoviesInteractorTests: XCTestCase {
-//    // MARK: Subject under test
+//  Created by Miguel Pimentel on 25/09/19.
+//  Copyright (c) 2019 Miguel Pimentel. All rights reserved.
 //
-//    var sut: ShowMoviesInteractor!
-//
-//    // MARK: Test lifecycle
-//
-//    override func setUp() {
-//        super.setUp()
-//        setupShowMoviesInteractor()
-//    }
-//
-//    override func tearDown() {
-//        super.tearDown()
-//    }
-//
-//    // MARK: Test setup
-//
-//    func setupShowMoviesInteractor() {
-//        sut = ShowMoviesInteractor()
-//    }
-//
-//    // MARK: Test doubles
-//
-//    class ShowMoviesPresentationLogicSpy: ShowMoviesPresentationLogic {
-//        var presentSomethingCalled = false
-//
-//        func presentSomething(response: ShowMovies.Something.Response) {
-//            presentSomethingCalled = true
-//        }
-//    }
-//
-//    // MARK: Tests
-//
-//    func testDoSomething() {
-//        // Given
-//        let spy = ShowMoviesPresentationLogicSpy()
-//        sut.presenter = spy
-//        let request = ShowMovies.Something.Request()
-//
-//        // When
-//        sut.doSomething(request: request)
-//
-//        // Then
-//        XCTAssertTrue(spy.presentSomethingCalled, "doSomething(request:) should ask the presenter to format the result")
-//    }
-//}
+
+@testable import Movs
+import XCTest
+
+class ShowMoviesInteractorTests: XCTestCase {
+  
+    // MARK: - Subject under test
+    
+    var sut: ShowMoviesInteractor!
+    var spy: ShowMoviesPresentationLogicSpy?
+    
+    let expectation = XCTestExpectation(description: "Movie Info Fetched")
+    
+    // MARK: - Test lifecycle
+    
+    override func setUp() {
+        super.setUp()
+        setupShowMoviesInteractor()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: - Test setup
+    
+    func setupShowMoviesInteractor() {
+        sut = ShowMoviesInteractor()
+        spy = ShowMoviesPresentationLogicSpy(tester: self)
+        sut.presenter = spy
+    }
+    
+    // MARK:  - Test doubles
+    
+    class ShowMoviesPresentationLogicSpy: ShowMoviesPresentationLogic {
+   
+        var tester: ShowMoviesInteractorTests?
+        
+        init(tester: ShowMoviesInteractorTests) {
+            self.tester = tester
+        }
+        
+        var presenteMoviesCalled: Bool? {
+            didSet {
+                self.tester?.expectation.fulfill()
+            }
+        }
+        
+        var presentQueriedMoviesCalled: Bool? {
+            didSet {
+                self.tester?.expectation.fulfill()
+            }
+        }
+        
+        func presentMovies(response: ShowMovies.fetchMovies.Response) {
+            self.presenteMoviesCalled = true
+        }
+        
+        func presentQueriedMovies(response: ShowMovies.queryMovies.Response) {
+            self.presentQueriedMoviesCalled = true
+        }
+    }
+    
+    // MARK: - Tests
+    
+    func testFetchMovieDetails() {
+        let fetchMoviesRequest = ShowMovies.fetchMovies.Request(page: 1)
+        let fetchQueriedRequest = ShowMovies.queryMovies.Request(keyword: "")
+        
+        sut.fetchData(request: fetchMoviesRequest)
+        sut.queryMovies(request: fetchQueriedRequest)
+        
+        wait(for: [self.expectation], timeout: 20)
+    }
+}
--- MovsTests/Scenes/ShowMovies/ShowMoviesPresenterTests.swift
+++ MovsTests/Scenes/ShowMovies/ShowMoviesPresenterTests.swift
@@ -1,58 +1,66 @@
-////
-////  ShowMoviesPresenterTests.swift
-////  Movs
-////
-////  Created by Miguel Pimentel on 25/09/19.
-////  Copyright (c) 2019 Miguel Pimentel. All rights reserved.
-////
 //
-//@testable import Movs
-//import XCTest
+//  ShowMoviesPresenterTests.swift
+//  Movs
 //
-//class ShowMoviesPresenterTests: XCTestCase {
-//    // MARK: Subject under test
+//  Created by Miguel Pimentel on 25/09/19.
+//  Copyright (c) 2019 Miguel Pimentel. All rights reserved.
 //
-//    var sut: ShowMoviesPresenter!
-//
-//    // MARK: Test lifecycle
-//
-//    override func setUp() {
-//        super.setUp()
-//        setupShowMoviesPresenter()
-//    }
-//
-//    override func tearDown() {
-//        super.tearDown()
-//    }
-//
-//    // MARK: Test setup
-//
-//    func setupShowMoviesPresenter() {
-//        sut = ShowMoviesPresenter()
-//    }
-//
-//    // MARK: Test doubles
-//
-//    class ShowMoviesDisplayLogicSpy: ShowMoviesDisplayLogic {
-//        var displaySomethingCalled = false
-//
-//        func displaySomething(viewModel: ShowMovies.Something.ViewModel) {
-//            displaySomethingCalled = true
-//        }
-//    }
-//
-//    // MARK: Tests
-//
-//    func testPresentSomething() {
-//        // Given
-//        let spy = ShowMoviesDisplayLogicSpy()
-//        sut.viewController = spy
-//        let response = ShowMovies.Something.Response()
-//
-//        // When
-//        sut.presentSomething(response: response)
-//
-//        // Then
-//        XCTAssertTrue(spy.displaySomethingCalled, "presentSomething(response:) should ask the view controller to display the result")
-//    }
-//}
+
+@testable import Movs
+import XCTest
+
+class ShowMoviesPresenterTests: XCTestCase {
+    
+    // MARK: Subject under test
+    
+    var sut: ShowMoviesPresenter!
+    let spy: ShowMoviesDisplayLogicSpy = ShowMoviesDisplayLogicSpy()
+    
+    // MARK: Test Lifecycle
+    
+    override func setUp() {
+        super.setUp()
+        setupShowMoviePresenter()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: Test setup
+    
+    func setupShowMoviePresenter() {
+        sut = ShowMoviesPresenter()
+        sut.viewController = spy
+    }
+    
+    // MARK: Test
+    
+    class ShowMoviesDisplayLogicSpy: ShowMoviesDisplayLogic {
+        
+        var displayMoviesCalled = false
+        var displayQueriedMoviesCalled = false
+        
+        func displayMovies(viewModel: ShowMovies.fetchMovies.ViewModel) {
+            self.displayMoviesCalled = true
+        }
+        
+        func displayQueriedMovies(viewModel: ShowMovies.queryMovies.ViewModel) {
+            self.displayQueriedMoviesCalled = true
+        }
+    }
+    
+    // MARK: Tests
+    
+    func testPresentMovieDetail() {
+        
+        let fetchMoviesResponse = ShowMovies.fetchMovies.Response(content: [Any]())
+        let fetchQueriedMoviesResponse = ShowMovies.queryMovies.Response(content: [Any]())
+    
+        sut.presentMovies(response: fetchMoviesResponse)
+        sut.presentQueriedMovies(response: fetchQueriedMoviesResponse)
+        
+        XCTAssertTrue(spy.displayMoviesCalled, "displayMovies(viewModel:) should ask the view controller to display the result")
+        XCTAssertTrue(spy.displayQueriedMoviesCalled, "displayMoviesCalled(viewMdel:) should ask the view controller to display the result")
+    }
+}

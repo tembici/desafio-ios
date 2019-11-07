c57b96dd4d6e2b17692599c55aa8422edb87f96f
Victor Rodrigues <victor.malta2012@gmail.com>
2019-11-06 23:04:10 -0300

    Initial Project

---
+++ Desafio_TemBici/Desafio_TemBici/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  Desafio_TemBici
+//
+//  Created by Victor Rodrigues on 06/11/19.
+//  Copyright © 2019 Victor Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+}
+
---
+++ Desafio_TemBici/Desafio_TemBici/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Desafio_TemBici
+//
+//  Created by Victor Rodrigues on 06/11/19.
+//  Copyright © 2019 Victor Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+

038864bd69fcdf1c0d968915e254abe337e26b84
Vand <vandcarlosjunior@gmail.com>
2020-05-08 18:24:20 -0300

    Fixed exec permission on test script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 xcodebuild -workspace TFilmes.xcworkspace \
             -scheme TFilmes \
             -destination platform=iOS\ Simulator,OS=13.4.1,name=iPhone\ 11 \
-            clean test
+            clean test | xcpretty

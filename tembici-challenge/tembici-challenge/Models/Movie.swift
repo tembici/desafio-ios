3b1d141a0fc868337f16725a09460e2d1bd596a7
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-14 17:28:50 -0300

    create Movie Model

---
+++ tembici-challenge/tembici-challenge/Models/Movie.swift
@@ -0,0 +1,57 @@
+//
+//  Movie.swift
+//  tembici-challenge
+//
+//  Created by Hannah  on 14/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Movies
+struct Movies:  Hashable, Codable {
+    let page, totalResults, totalPages: Int
+    let moviesList: [Movie]
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case moviesList = "results"
+    }
+}
+
+
+// MARK: - Result
+struct Movie: Hashable, Codable, Identifiable {
+    let popularity: Double
+    let voteCount: Int
+    let video: Bool
+    let posterPath: String?
+    let id: Int
+    let adult: Bool
+    let backdropPath: String?
+    let originalLanguage, originalTitle: String
+    let genreIDS: [Int]
+    let title: String
+    let voteAverage: Double
+    let overview, releaseDate: String
+
+    enum CodingKeys: String, CodingKey {
+        case popularity
+        case voteCount = "vote_count"
+        case video
+        case posterPath = "poster_path"
+        case id, adult
+        case backdropPath = "backdrop_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case title
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+        
+    }
+}
+

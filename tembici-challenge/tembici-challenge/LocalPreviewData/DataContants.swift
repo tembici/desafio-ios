13ce2a0906ec068b060abe89e6ddba8e2358081d
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-14 17:55:11 -0300

    create file to load local data from preview

---
+++ tembici-challenge/tembici-challenge/LocalPreviewData/DataContants.swift
@@ -0,0 +1,15 @@
+//
+//  DataContants.swift
+//  tembici-challenge
+//
+//  Created by Hannah  on 14/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import Foundation
+
+///class responsible for providing static data for use in preview
+class DataContants {
+    static let sharedInstance = DataContants()
+     let urlImage =  #"\/f4aul3FyD3jv3v4bul1IrkWZvzq.jpg"#
+}

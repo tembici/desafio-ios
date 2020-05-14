d4280c221648b7cf4bf7005c1820572c6d6c8a9f
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-14 17:33:06 -0300

    create MovieListView and FavoritesListView

---
+++ tembici-challenge/tembici-challenge/Views/FavoritesListView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListView.swift
+//  tembici-challenge
+//
+//  Created by Hannah  on 14/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoritesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).background(Color.green)
+    }
+}
+
+struct FavoritesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesListView()
+    }
+}
---
+++ tembici-challenge/tembici-challenge/Views/MoviesListView.swift
@@ -0,0 +1,21 @@
+//
+//  MoviesListView.swift
+//  tembici-challenge
+//
+//  Created by Hannah  on 14/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct MoviesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).background(Color.red)
+    }
+}
+
+struct MoviesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesListView()
+    }
+}

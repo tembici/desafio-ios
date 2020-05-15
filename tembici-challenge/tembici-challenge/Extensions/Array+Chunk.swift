bf36f73270b466ba6312954e1a3158bf630c6ac7
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-14 21:14:46 -0300

    add extension to divide array

---
+++ tembici-challenge/tembici-challenge/Extensions/Array+Chunk.swift
@@ -0,0 +1,27 @@
+//
+//  Array+Chunk.swift
+//  tembici-challenge
+//
+//  Created by Hannah  on 14/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+extension Array {
+    func chunked(into size:Int) -> [[Movie]] {
+        
+        var chunkedArray = [[Movie]]()
+        
+        for index in 0...self.count {
+            if index % size == 0 && index != 0 {
+                chunkedArray.append(Array(self[(index - size)..<index]) as! [Movie])
+            } else if(index == self.count && chunkedArray.count>0) {
+                chunkedArray.append(Array(self[index - 1..<index]) as! [Movie])
+            }
+        }
+        return chunkedArray
+    }
+}

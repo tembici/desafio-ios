d0e7c20e2daa41b3f4f5d60a59459a85c88ff3b5
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-14 22:55:31 -0300

    custom layout Movie Detail

--- tembici-challenge/tembici-challenge/Views/MovieDetailView.swift
+++ tembici-challenge/tembici-challenge/Views/MovieDetailView.swift
@@ -10,18 +10,57 @@ import SwiftUI
 
 struct MovieDetailView: View {
     
+    @ObservedObject var movieDetailVM: MovieDetailViewModel
+    
     init(movie: Movie) {
-        
+        movieDetailVM = MovieDetailViewModel(movie: movie)
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            
+            VStack {
+                VStack() {
+                    ImageViewComponent(url: movieDetailVM.urlImage, type:.backdrop)
+                    HStack {
+                        VStack(alignment: .center, spacing: 8) {
+                            Text(movieDetailVM.title)
+                                .fontWeight(.heavy)
+                                .lineLimit(nil)
+                                .font(.custom(Constants.Design.Font.Thonburi, size: 16))
+                                .foregroundColor(Color.white)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }
+                        Spacer()
+                        Button(action: {
+                            
+                            
+                        }) {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow).padding(5)
+                                .font(.system(size: 30))
+                        }       .padding(.trailing, 10)
+                        
+                    }.padding(10)
+                        .padding(.leading, 10)
+                }
+                VStack(alignment: .leading) {
+                    
+                    Text("Relase Date: \(movieDetailVM.date)")
+                        .padding()
+                    
+                    Text(movieDetailVM.overview).textStyle(ContentStyle())
+                }
+                Spacer()
+                
+            }
+        }.navigationBarTitle("", displayMode: .inline)
     }
 }
 
 struct MovieDetailView_Previews: PreviewProvider {
     @State static var movie = DataContants.sharedInstance.movieModelPreview
-
+    
     static var previews: some View {
         MovieDetailView(movie:movie)
     }

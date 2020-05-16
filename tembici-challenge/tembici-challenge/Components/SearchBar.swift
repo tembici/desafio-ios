3dcfa8f7f1de7feec9a8316ab3e734092a857533
Hannah Lisboa <hannahlisboa@outlook.com>
2020-05-16 20:40:59 -0300

    Create SearchBar.swift

---
+++ tembici-challenge/tembici-challenge/Components/SearchBar.swift
@@ -0,0 +1,58 @@
+//
+//  SearchBar.swift
+//  tembici-challenge
+//
+//  Created by Hannah  on 16/05/2020.
+//  Copyright © 2020 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+    var placeholder: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.endEditing(true)
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.placeholder = placeholder
+        searchBar.searchBarStyle = .prominent
+        searchBar.autocapitalizationType = .none
+//        searchBar.backgroundColor = UIColor.blue;
+//        searchBar.barTintColor = UIColor.black;
+        searchBar.barStyle = .black
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
